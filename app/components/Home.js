6dcb559b3547dbdebfbdaffda33fc7df33f45346
Stefan Machmeier <stefan-machmeier@outlook.com>
2018-12-17 21:02:21 +0100

    Add icon

--- app/components/Home.js
+++ app/components/Home.js
@@ -10,9 +10,7 @@ import {
   CardBody,
   CardTitle,
   Card,
-  CardImg,
-  Row,
-  Col
+  CardImg
 } from 'reactstrap';
 import routes from '../constants/routes';
 
@@ -27,28 +25,23 @@ export default class Home extends Component<Props> {
         <Breadcrumb tag="nav" listTag="div">
           <BreadcrumbItem>Home</BreadcrumbItem>
         </Breadcrumb>
-        <Row>
-          <Col sm={2}>
-            <Card>
-              <CardImg
-                top
-                width="100%"
-                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                alt="Card image cap"
-              />
-              <CardBody>
-                <CardTitle>Scrum Retroboard</CardTitle>
-                <CardText>
-                  Enhance your Retrospective with our very mucho awesome bine
-                  board.
-                </CardText>
-                <Button color="link" innerRef={routes.BOARD}>
-                  <Link to={routes.BOARD}>to the Board</Link>
-                </Button>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>Scrum Retroboard</CardTitle>
+            <CardText>
+              Enhance your Retrospective with our very mucho awesome bine board.
+            </CardText>
+            <Button color="link" innerRef={routes.BOARD}>
+              <Link to={routes.BOARD}>to the Board</Link>
+            </Button>
+          </CardBody>
+        </Card>
       </Container>
     );
   }
