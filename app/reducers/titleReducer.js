e3ad7e5e5827a3cdbe146eff7526646408496f27
Stefan Machmeier <stefan-machmeier@outlook.com>
2019-05-03 22:14:07 +0200

    Clean up initial state of reducers

--- app/reducers/titleReducer.js
+++ app/reducers/titleReducer.js
@@ -3,7 +3,7 @@ import * as types from '../actions/actionTypes';
 import initialState from '../constants/initialState';
 import type { Action } from './types';
 
-const title = (state: object = initialState.points, action: Action) => {
+const title = (state: object = initialState.title, action: Action) => {
   switch (action.type) {
     case types.UPDATE_TITLE:
       return {
